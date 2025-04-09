024a714c3e8346c3c2d7402f0f9116deca4e287d
ajarms <ajarms11@gmail.com>
2025-04-08 20:02:58 -0500

    scoring, music, retry

--- godot/scripts/Main.cs
+++ godot/scripts/Main.cs
@@ -6,6 +6,16 @@ public partial class Main : Node
 	[Export]
 	public PackedScene MobScene { get; set; }
 
+    public override void _Ready(){
+        GetNode<Control>("UserInterface/Retry").Hide();
+    }
+
+    public override void _UnhandledInput(InputEvent @event){
+        if (@event.IsActionPressed("ui_accept") && GetNode<Control>("UserInterface/Retry").Visible){
+			GetTree().ReloadCurrentScene();
+		}
+    }
+
 	private void OnMobTimerTimeout(){
 		Mob mob = MobScene.Instantiate<Mob>();
 
@@ -16,10 +26,13 @@ public partial class Main : Node
 		Vector3 playerPosition = this.GetNode<Player>("Player").Position;
 		mob.Initialize(mobSpawnLocation.Position, playerPosition);
 
+		mob.Squashed += GetNode<ScoreLabel>("UserInterface/ScoreLabel").OnMobSquashed;
+
 		AddChild(mob);
 	}
 
 	private void OnPlayerHit(){
 		this.GetNode<Timer>("MobTimer").Stop();
+        GetNode<Control>("UserInterface/Retry").Show();
 	}
 }
---
+++ godot/scripts/ScoreLabel.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class ScoreLabel : Label
+{
+    private int score = 0;
+
+    public void OnMobSquashed(){
+        score += 1;
+        this.Text = $"Score: {score}";
+    }
+}
